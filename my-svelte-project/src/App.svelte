eb78da63fc4b2d71200937459f4c8dd8047a5cf9
Alex Gréau <alexandre.greau@triple-a.io>
2024-05-07 16:11:01 +0800

    Init with daisy working

--- my-svelte-project/src/App.svelte
+++ my-svelte-project/src/App.svelte
@@ -5,27 +5,16 @@
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
 
   <div class="card">
     <Counter />
   </div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
+  <button class="btn btn-primary">Button</button>
 
-  <p class="read-the-docs underline">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
